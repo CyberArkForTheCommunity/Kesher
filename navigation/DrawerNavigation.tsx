47ada778324c3ba36e4b2a1a4c3da5d7cb22d625
Ory Tabak <ory.tabak@cyberark.com>
2021-04-06 16:03:16 +0300

    merge

--- navigation/DrawerNavigation.tsx
+++ navigation/DrawerNavigation.tsx
@@ -10,6 +10,7 @@ import useUserRole from "../hooks/useUserRoles";
 import Board from "../screens/Board";
 import Corona from "../screens/Corona";
 import Home from "../screens/Home";
+import Presence from "../screens/Presence";
 import Reports from "../screens/Reports";
 import ReportSubCategory from "../screens/ReportSubCategory";
 import Setting from "../screens/Setting";
@@ -59,6 +60,14 @@ const AppDrawer = () => {
               drawerLabel: () => <DrawerMenu title="דיווח יומי" />,
             }}
             component={ReportNavigator}
+          />
+          <Drawer.Screen
+            name="Presence"
+            options={{
+              drawerIcon: () => <Image style={styles.icon} source={require("../assets/images/presence.png")} />,
+              drawerLabel: () => <DrawerMenu title="עידכון נוכחות" />,
+            }}
+            component={Presence}
           />
           <Drawer.Screen
             name="Messages"
