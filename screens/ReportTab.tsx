30baff4804ad9272e29d89fb0cf3066fd356e123
moshik-cyberark <moshik.yerushalmi@cyberark.com>
2021-04-06 15:03:32 +0300

    fix report flow

--- screens/ReportTab.tsx
+++ screens/ReportTab.tsx
@@ -101,7 +101,8 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
+    width: "100%"
   },
   categoryPic: {
     height: 120,
